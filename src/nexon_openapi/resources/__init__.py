a97c03e2dd5c2f34272780fd30f21ca8a9cb6da6
BlueWhaleKo <koko8624@gmail.com>
2023-12-17 17:11:45 +0900

    feat: NexonAPIClientAsync and BaramAsync

--- src/nexon_openapi/resources/__init__.py
+++ src/nexon_openapi/resources/__init__.py
@@ -2,7 +2,7 @@ from ._wars_of_prasia import WarsOfPrasia as WarsOfPrasia
 from ._mabinogi_heroes import MabinogiHeroes as MabinogiHeroes
 from ._crazy_arcade import CrazyArcade as CrazyArcade
 from ._maplestorym import MapleStoryM as MapleStoryM
-from ._baram import Baram as Baram
+from ._baram import Baram as Baram, BaramAsync as BaramAsync
 from ._baramy import BaramY as BaramY
 from ._kartrush import KartRiderRushPlus as KartRiderRushPlus
 from ._hit2 import Hit2 as Hit2
--- src/nexon_openapi/resources/_baram.py
+++ src/nexon_openapi/resources/_baram.py
@@ -10,11 +10,11 @@ from ._types import Ocid
 from .._types import NOT_GIVEN, Body, Query, Headers, NotGiven
 from .._models import BaseModel
 from ..utils import maybe_transform
-from .._resource import SyncAPIResource
+from .._resource import SyncAPIResource, AsyncAPIResource
 from .._base_client import make_request_options
 
 if TYPE_CHECKING:
-    from .._client import NexonOpenAPI
+    from .._client import NexonOpenAPI, NexonOpenAPIAsync
 
 
 class Baram(SyncAPIResource):
@@ -175,6 +175,164 @@ class Baram(SyncAPIResource):
         )
 
 
+class BaramAsync(AsyncAPIResource):
+    def __init__(self, client: NexonOpenAPIAsync) -> None:
+        super().__init__(client)
+
+    async def get_ocid(
+        self,
+        *,
+        server_name: str,
+        character_name: str,
+        extra_headers: Optional[Headers] = None,
+        extra_query: Optional[Query] = None,
+        extra_body: Optional[Body] = None,
+        timeout: Union[float, httpx.Timeout, None, NotGiven] = NOT_GIVEN,
+    ) -> str:
+        response = await self._get(
+            "baram/v1/id",
+            options=make_request_options(
+                query=maybe_transform(
+                    {"server_name": server_name, "character_name": character_name},
+                    GetOcidRequestParam,
+                ),
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+            ),
+            cast_to=Ocid,
+        )
+
+        return response.ocid
+
+    async def get_character_basic(
+        self,
+        *,
+        ocid: str,
+        extra_headers: Optional[Headers] = None,
+        extra_query: Optional[Query] = None,
+        extra_body: Optional[Body] = None,
+        timeout: Union[float, httpx.Timeout, None, NotGiven] = NOT_GIVEN,
+    ) -> BaramCharacterBasic:
+        return await self._get(
+            path="baram/v1/character/basic",
+            options=make_request_options(
+                query=maybe_transform({"ocid": ocid}, GetCharacterBasicRequestParam),
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+            ),
+            cast_to=BaramCharacterBasic,
+        )
+
+    async def get_character_title(
+        self,
+        *,
+        ocid: str,
+        extra_headers: Optional[Headers] = None,
+        extra_query: Optional[Query] = None,
+        extra_body: Optional[Body] = None,
+        timeout: Union[float, httpx.Timeout, None, NotGiven] = NOT_GIVEN,
+    ) -> BaramCharacterTitle:
+        return await self._get(
+            path="baram/v1/character/title",
+            options=make_request_options(
+                query=maybe_transform({"ocid": ocid}, GetCharacterTitleRequestParam),
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+            ),
+            cast_to=BaramCharacterTitle,
+        )
+
+    async def get_character_title_equipment(
+        self,
+        *,
+        ocid: str,
+        extra_headers: Optional[Headers] = None,
+        extra_query: Optional[Query] = None,
+        extra_body: Optional[Body] = None,
+        timeout: Union[float, httpx.Timeout, None, NotGiven] = NOT_GIVEN,
+    ) -> BaramCharacterTitleEquipment:
+        return await self._get(
+            path="baram/v1/character/title-equipment",
+            options=make_request_options(
+                query=maybe_transform({"ocid": ocid}, GetCharacterTitleEquipmentRequestParam),
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+            ),
+            cast_to=BaramCharacterTitleEquipment,
+        )
+
+    async def get_character_item_equipment(
+        self,
+        *,
+        ocid: str,
+        extra_headers: Optional[Headers] = None,
+        extra_query: Optional[Query] = None,
+        extra_body: Optional[Body] = None,
+        timeout: Union[float, httpx.Timeout, None, NotGiven] = NOT_GIVEN,
+    ) -> BaramCharacterItemEquipment:
+        return await self._get(
+            path="baram/v1/character/item-equipment",
+            options=make_request_options(
+                query=maybe_transform({"ocid": ocid}, GetCharacterItemEquipmentRequestParam),
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+            ),
+            cast_to=BaramCharacterItemEquipment,
+        )
+
+    async def get_character_stat(
+        self,
+        *,
+        ocid: str,
+        extra_headers: Optional[Headers] = None,
+        extra_query: Optional[Query] = None,
+        extra_body: Optional[Body] = None,
+        timeout: Union[float, httpx.Timeout, None, NotGiven] = NOT_GIVEN,
+    ) -> BaramCharacterStat:
+        return await self._get(
+            path="baram/v1/character/stat",
+            options=make_request_options(
+                query=maybe_transform({"ocid": ocid}, GetCharacterStatRequestParam),
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+            ),
+            cast_to=BaramCharacterStat,
+        )
+
+    async def get_character_guild(
+        self,
+        *,
+        ocid: str,
+        extra_headers: Optional[Headers] = None,
+        extra_query: Optional[Query] = None,
+        extra_body: Optional[Body] = None,
+        timeout: Union[float, httpx.Timeout, None, NotGiven] = NOT_GIVEN,
+    ) -> BaramCharacterGuild:
+        return await self._get(
+            path="baram/v1/character/guild",
+            options=make_request_options(
+                query=maybe_transform({"ocid": ocid}, GetCharacterGuildRequestParam),
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+            ),
+            cast_to=BaramCharacterGuild,
+        )
+
+
 class GetOcidRequestParam(TypedDict, total=True):
     server_name: Required[str]
     character_name: Required[str]
@@ -222,14 +380,14 @@ class GetCharacterTitleRequestParam(TypedDict, total=False):
 
 class BaramCharacterTitle(BaseModel):
     title: List[Title]
-    
+
     class Title(BaseModel):
         title_id: str
         """ 칭호 명 """
 
         date_expire: Optional[str]
         """ 칭호 만료 일(시) (UTC0) """
-        
+
 
 class GetCharacterTitleEquipmentRequestParam(TypedDict, total=False):
     ocid: Required[str]
@@ -237,14 +395,14 @@ class GetCharacterTitleEquipmentRequestParam(TypedDict, total=False):
 
 class BaramCharacterTitleEquipment(BaseModel):
     title_equipment: List[TitleEquipment]
-    
+
     class TitleEquipment(BaseModel):
         title_id: str
         """ 장착 칭호 명 """
 
         date_expire: Optional[str]
         """ 칭호 만료 일(시) (UTC0) """
-        
+
 
 class GetCharacterItemEquipmentRequestParam(TypedDict, total=False):
     ocid: Required[str]
@@ -252,14 +410,14 @@ class GetCharacterItemEquipmentRequestParam(TypedDict, total=False):
 
 class BaramCharacterItemEquipment(BaseModel):
     item_equipment: List[ItemEquipment]
-    
+
     class ItemEquipment(BaseModel):
         item_id: str
         """ 장착 아이템 명 """
 
         item_equipment_slot_name: str
         """ 장착 아이템 페이즈 슬롯 명 """
-        
+
 
 class GetCharacterStatRequestParam(TypedDict, total=False):
     ocid: Required[str]
@@ -267,14 +425,14 @@ class GetCharacterStatRequestParam(TypedDict, total=False):
 
 class BaramCharacterStat(BaseModel):
     stat: List[Stat]
-    
+
     class Stat(BaseModel):
         stat_name: str
         """ 능력치 명 """
 
-        stat_value : str
+        stat_value: str
         """ 능력치 값 """
-        
+
 
 class GetCharacterGuildRequestParam(TypedDict, total=False):
     ocid: Required[str]
